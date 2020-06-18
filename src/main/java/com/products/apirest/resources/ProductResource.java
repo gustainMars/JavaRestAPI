04cf6daa70540b60f28f6996285a9d410b4635b6
gustasDesktop <gustaDesktop@github.com>
2020-06-17 23:09:20 -0300

    I implemented the DELETE and PUT methods in ProductResource.java, using the @DeleteMapping and @PutMapping annotations.

--- src/main/java/com/products/apirest/resources/ProductResource.java
+++ src/main/java/com/products/apirest/resources/ProductResource.java
@@ -6,11 +6,13 @@ import com.products.apirest.models.Product;
 import com.products.apirest.repository.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -36,4 +38,26 @@ public class ProductResource {
         return repository.save(product);
     }
     
+    /* Implementation carried out in the course. 
+    However, I believe that it would be a best practice 
+    to request the id and, through it, the method to effect 
+    the deletion.
+
+    @DeleteMapping("/product")
+    public void deleteProduct(@RequestBody Product product) {
+        repository.delete(product);
+    } 
+    */
+
+    @DeleteMapping("/product/{id}")
+    public void deleteProduct(@PathVariable(value="id") long id) {
+        Product product = new Product();
+        product.setId(id);
+        repository.delete(product);
+    } 
+
+    @PutMapping("/product") 
+    public Product changeProduct(@RequestBody Product product) {
+        return repository.save(product);        
+    }
 }
